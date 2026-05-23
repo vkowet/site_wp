833595f9de53b6a5bdb8460ec442c4000237ae18
vkowet <38705706+vkowet@users.noreply.github.com>
2026-05-24 01:40:59 +0200

    Update world-presence-admin.js

--- assets/js/world-presence-admin.js
+++ assets/js/world-presence-admin.js
@@ -1,1 +1,42 @@
-jQuery(document).ready(function($){let frame;$('.fms-upload-image').on('click',function(e){e.preventDefault();const button=$(this);if(frame){frame.open();return;}frame=wp.media({title:'Choisir une image',button:{text:'Utiliser cette image'},multiple:false});frame.on('select',function(){const attachment=frame.state().get('selection').first().toJSON();button.prev('.fms-image-id').val(attachment.id);button.siblings('.fms-image-preview').html('<img src="'+attachment.url+'" style="max-width:150px;height:auto;" />');});frame.open();});$('.fms-remove-image').on('click',function(e){e.preventDefault();$(this).siblings('.fms-image-id').val('');$(this).siblings('.fms-image-preview').html('');});});
+jQuery(document).ready(function ($) {
+
+    $('.fms-upload-image').on('click', function (e) {
+        e.preventDefault();
+
+        const button = $(this);
+        const imageField = button.prev('.fms-image-id');
+        const preview = button.siblings('.fms-image-preview');
+
+        const frame = wp.media({
+            title: 'Choisir une image',
+            button: {
+                text: 'Utiliser cette image'
+            },
+            multiple: false
+        });
+
+        frame.on('select', function () {
+            const attachment = frame.state().get('selection').first().toJSON();
+
+            imageField.val(attachment.id);
+
+            preview.html(
+                '<img src="' + attachment.url + '" style="max-width:150px;height:auto;" />'
+            );
+        });
+
+        frame.open();
+    });
+
+    $('.fms-remove-image').on('click', function (e) {
+        e.preventDefault();
+
+        const button = $(this);
+        const imageField = button.siblings('.fms-image-id');
+        const preview = button.siblings('.fms-image-preview');
+
+        imageField.val('');
+        preview.html('');
+    });
+
+});
