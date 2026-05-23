764869b06452ee6f3f3c2f280b1b2bd0b279d6e3
vkowet <38705706+vkowet@users.noreply.github.com>
2026-05-24 00:59:27 +0200

    Add world-presence.css stylesheet to enqueue assets

--- functions.php
+++ functions.php
@@ -19,6 +19,7 @@ function fms_enqueue_assets() {
 
     wp_enqueue_script('fms-navigation', get_template_directory_uri() . '/assets/js/navigation.js', [], wp_get_theme()->get('Version'), true);
     wp_enqueue_script('fms-hero-slider', get_template_directory_uri() . '/assets/js/hero-slider.js', [], wp_get_theme()->get('Version'), true);
+    wp_enqueue_style('fms-world', get_template_directory_uri() . '/assets/css/world-presence.css', [], wp_get_theme()->get('Version'));
 }
 add_action('wp_enqueue_scripts', 'fms_enqueue_assets');
 
