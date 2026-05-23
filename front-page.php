5b87e7918b44160da26e80a4cc517555afa8f11e
vkowet <38705706+vkowet@users.noreply.github.com>
2026-05-24 00:54:59 +0200

    Add home-world-presence template part to front page

--- front-page.php
+++ front-page.php
@@ -32,4 +32,5 @@ $link=get_theme_mod('fms_slide_'.$i.'_button_link','#'); ?>
 <?php endif; ?>
 <?php get_template_part('template-parts/home-message'); ?>
 <?php get_template_part('template-parts/home-foundress'); ?>
+<?php get_template_part('template-parts/home-world-presence'); ?>
 <?php get_footer(); ?>
