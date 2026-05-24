0d7fc2fa7698aa7939fb82e638035989944ee75b
vkowet <38705706+vkowet@users.noreply.github.com>
2026-05-24 16:30:57 +0200

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -33,4 +33,5 @@ $link=get_theme_mod('fms_slide_'.$i.'_button_link','#'); ?>
 <?php get_template_part('template-parts/home-message'); ?>
 <?php get_template_part('template-parts/home-foundress'); ?>
 <?php get_template_part('template-parts/home-world-presence'); ?>
+<?php get_template_part('template-parts/home-stats'); ?>
 <?php get_footer(); ?>
