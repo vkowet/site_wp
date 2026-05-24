3b6157fa7390a3720c2f8a87ee26641aad489a73
vkowet <38705706+vkowet@users.noreply.github.com>
2026-05-24 23:00:50 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,20 @@
+<?php
+$name = get_option('fms_footer_name', 'Sœurs Franciscaines Servantes de Marie');
+$address1 = get_option('fms_footer_address1', 'Généralat');
+$address2 = get_option('fms_footer_address2', '15 rue Monin');
+$city = get_option('fms_footer_city', '41000 BLOIS');
+$phone = get_option('fms_footer_phone', '');
+$email = get_option('fms_footer_email', '');
+
+$facebook = get_option('fms_footer_facebook', '');
+$youtube = get_option('fms_footer_youtube', '');
+$instagram = get_option('fms_footer_instagram', '');
+$world_link = get_option('fms_footer_world_link', '');
+
+$copyright = get_option('fms_footer_copyright', '© Franciscaines Servantes de Marie');
+$bottom_text = get_option('fms_footer_bottom_text', 'Au service de la mission dans le monde');
+?>
+
 <footer class="fms-footer">
     <div class="fms-footer-cta">
         <div>
@@ -8,10 +25,11 @@
     </div>
 
     <div class="fms-footer-main">
+
         <div class="fms-footer-col fms-footer-identity">
-            <h3>Franciscaines Servantes de Marie</h3>
-            <p class="fms-footer-subtitle">Siège mondial – Généralat</p>
-            <p>Congrégation religieuse présente dans le monde, au service de la mission, de l’éducation et de l’Évangile.</p>
+            <h3><?php echo esc_html($name); ?></h3>
+            <p class="fms-footer-subtitle"><?php echo esc_html($address1); ?></p>
+            <p><?php echo esc_html($bottom_text); ?></p>
         </div>
 
         <div class="fms-footer-col">
@@ -28,11 +46,18 @@
         <div class="fms-footer-col">
             <h4>Généralat</h4>
             <address>
-                Sœurs Franciscaines Servantes de Marie<br>
-                Généralat<br>
-                15 rue Monin<br>
-                41000 BLOIS<br>
-                France
+                <?php echo esc_html($name); ?><br>
+                <?php echo esc_html($address1); ?><br>
+                <?php echo esc_html($address2); ?><br>
+                <?php echo esc_html($city); ?><br>
+
+                <?php if ($phone): ?>
+                    Tél : <?php echo esc_html($phone); ?><br>
+                <?php endif; ?>
+
+                <?php if ($email): ?>
+                    Email : <a href="mailto:<?php echo esc_attr($email); ?>"><?php echo esc_html($email); ?></a>
+                <?php endif; ?>
             </address>
         </div>
 
@@ -41,19 +66,5 @@
             <ul>
                 <li><a href="<?php echo esc_url(home_url('/faire-un-don')); ?>">Faire un don</a></li>
                 <li><a href="<?php echo esc_url(home_url('/vocations')); ?>">Vocations</a></li>
-                <li><a href="<?php echo esc_url(home_url('/mentions-legales')); ?>">Mentions légales</a></li>
-                <li><a href="<?php echo esc_url(home_url('/politique-de-confidentialite')); ?>">Politique de confidentialité</a></li>
-                <li><a href="<?php echo esc_url(home_url('/plan-du-site')); ?>">Plan du site</a></li>
-            </ul>
-        </div>
-    </div>
 
-    <div class="fms-footer-bottom">
-        <p>&copy; <?php echo date('Y'); ?> Franciscaines Servantes de Marie – Tous droits réservés.</p>
-        <p>Site officiel du siège mondial – Blois, France</p>
-    </div>
-</footer>
-
-<?php wp_footer(); ?>
-</body>
-</html>
+                <?php if ($world_link):
