bb90fa37e860964cd52476d24e0721648a2a8c9d
vkowet <38705706+vkowet@users.noreply.github.com>
2026-05-24 21:30:01 +0200

    Create home-vocation.php

---
+++ template-parts/home-vocation.php
@@ -0,0 +1,20 @@
+<section class="fms-vocation-section">
+
+    <div class="fms-vocation-overlay">
+
+        <div class="fms-vocation-content">
+            <h2>RÉPONDRE À L’APPEL DU SERVICE</h2>
+
+            <p>
+                La vocation franciscaine est un chemin de foi, de service et
+                d’éducation, au service des plus fragiles et de la mission dans le monde.
+            </p>
+
+            <a href="<?php echo esc_url(home_url('/vocations')); ?>" class="fms-vocation-btn">
+                Découvrir les vocations
+            </a>
+        </div>
+
+    </div>
+
+</section>
