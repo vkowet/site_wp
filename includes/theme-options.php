5d13fd54de979fae9d8ac217bac1ba12db6f0d96
vkowet <38705706+vkowet@users.noreply.github.com>
2026-05-27 22:58:37 +0200

    Update theme-options.php

--- includes/theme-options.php
+++ includes/theme-options.php
@@ -219,7 +219,41 @@ fms_section_card_start('Pages internes');
 <tr><th>Marge top</th><td><input type="text" name="fms_theme_options[pages][margin_top]" value="<?php echo esc_attr(fms_get_option('pages','margin_top','5')); ?>"></td></tr>
 <tr><th>Largeur contenu</th><td><input type="text" name="fms_theme_options[pages][content_width]" value="<?php echo esc_attr(fms_get_option('pages','content_width','1100')); ?>"></td></tr>
 <tr><th>Overlay</th><td><input type="text" name="fms_theme_options[pages][overlay_opacity]" value="<?php echo esc_attr(fms_get_option('pages','overlay_opacity','0.45')); ?>"></td></tr>
+<tr>
+<th>Pages concernées</th>
+
+<td>
+
 <?php
+$selected_pages = fms_get_option('pages','selected_pages', []);
+
+$pages = get_pages();
+
+foreach($pages as $page):
+
+$checked = in_array($page->ID, $selected_pages) ? 'checked' : '';
+?>
+
+<label style="display:block;margin-bottom:8px;">
+
+<input type="checkbox"
+name="fms_theme_options[pages][selected_pages][]"
+value="<?php echo $page->ID; ?>"
+<?php echo $checked; ?>>
+
+<?php echo esc_html($page->post_title); ?>
+
+</label>
+
+<?php endforeach; ?>
+
+<p style="margin-top:10px;color:#666;">
+Choisissez les pages internes utilisant ce bandeau.
+</p>
+
+</td>
+</tr>
+    <?php
 fms_section_card_end();
 ?>
 <?php endif; ?>
