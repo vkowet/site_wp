61705690f1576d92c2b9ba36f4f244a2d75fc9de
vkowet <38705706+vkowet@users.noreply.github.com>
2026-05-27 21:49:07 +0200

    Add automatic system page templates

---
+++ page-mentions-legales.php
@@ -0,0 +1,7 @@
+<?php
+get_header();
+
+$title = fms_get_option('system','legal_title',get_the_title());
+$content = wpautop(fms_get_option('system','legal_content',get_the_content()));
+?>
+<section class="fms-system-page-hero"><div class="fms-system-page-overlay"><div class="fms-system-page-inner"><p class="fms-system-breadcrumb"><a href="<?php echo home_url(); ?>">Accueil</a><span>></span><?php echo esc_html($title); ?></p><h1><?php echo esc_html($title); ?></h1></div></div></section><section class="fms-system-page-content"><div class="fms-system-page-container"><?php echo $content; ?></div></section><?php get_footer(); ?>
