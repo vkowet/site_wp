b2c47f163338989c20fb0591e7c430e6868c9857
vkowet <38705706+vkowet@users.noreply.github.com>
2026-05-31 00:34:44 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -12,10 +12,11 @@ function fms_theme_setup() {
     add_theme_support('menus');
     add_theme_support('custom-logo');
 
-    register_nav_menus([
-        'top-menu'     => __('Menu supérieur', 'fms-theme'),
-        'primary-menu' => __('Menu principal', 'fms-theme'),
-    ]);
+   register_nav_menus([
+    'top-menu'     => __('Menu supérieur', 'fms-theme'),
+    'primary-menu' => __('Menu principal', 'fms-theme'),
+    'mobile-menu'  => __('Menu mobile', 'fms-theme'),
+]);
 }
 add_action('after_setup_theme', 'fms_theme_setup');
 
