c1ee791ec4cd92c5b5ef3cb417b3bd048aeab469
vkowet <valko.prod@gmail.com>
2026-06-03 23:52:46 +0200

    feat: amélioration page faire un don

--- functions.php
+++ functions.php
@@ -9,4 +9,3 @@ require_once get_template_directory() . '/inc/helpers.php';
 require_once get_template_directory() . '/inc/assets.php';
 require_once get_template_directory() . '/inc/system-pages.php';
 require_once get_template_directory() . '/inc/modules.php';
-require_once get_template_directory() . '/inc/donation.php';
