4feae673bc2a0fdf32f99914b6724fe4afa0f1f5
vkowet <valko.prod@gmail.com>
2026-06-07 16:06:12 +0200

    Amélioration page contact: liens mailto/tel, bloc formulaire

--- page-contact.php
+++ page-contact.php
@@ -3,6 +3,14 @@
 get_header();
 
 $contact = fms_get_contact();
+
+$post_content = '';
+if (have_posts()) {
+    while (have_posts()) {
+        the_post();
+        $post_content = trim(get_the_content());
+    }
+}
 ?>
 
 <main class="contact-page">
@@ -30,17 +38,30 @@ $contact = fms_get_contact();
             <?php if (!empty($contact['phone'])): ?>
                 <p>
                     <strong>Téléphone</strong><br>
-                    <?php echo esc_html($contact['phone']); ?>
+                    <a href="tel:<?php echo esc_attr( preg_replace('/[^0-9+]/', '', $contact['phone']) ); ?>">
+                        <?php echo esc_html($contact['phone']); ?>
+                    </a>
                 </p>
             <?php endif; ?>
 
             <?php if (!empty($contact['email'])): ?>
                 <p>
                     <strong>Courriel</strong><br>
-                     ?>">
+                    <a href="mailto:<?php echo esc_attr($contact['email']); ?>">
                         <?php echo esc_html($contact['email']); ?>
                     </a>
                 </p>
+            <?php endif; ?>
+        </div>
+
+        <div class="contact-form">
+            <h2>Nous écrire</h2>
+
+            <?php if (!empty($post_content)): ?>
+                <?php echo apply_filters('the_content', $post_content); ?>
+            <?php else: ?>
+                <p>Remplissez le formulaire ci-dessous pour nous contacter. Nous revenons vers vous dans les meilleurs délais.</p>
+                <?php echo do_shortcode('[contact-form-7 id="123" title="Contact"]'); ?>
             <?php endif; ?>
         </div>
 
